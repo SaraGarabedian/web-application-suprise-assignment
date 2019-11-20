161e395d6f26295310016dde5c01e97ab5e4da01
Pratima Shegokar <pratphad@gmail.com>
2019-11-20 21:56:56 +0100

    Add username to post and display it in frontend

--- src/main/java/se/kth/sda6/skeleton/posts/Post.java
+++ src/main/java/se/kth/sda6/skeleton/posts/Post.java
@@ -1,10 +1,13 @@
 package se.kth.sda6.skeleton.posts;
 
-import net.bytebuddy.dynamic.loading.InjectionClassLoader;
 import org.hibernate.validator.constraints.Length;
+import se.kth.sda6.skeleton.comments.Comment;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import java.util.ArrayList;
+import java.util.List;
+
 
 // @TODO add Hibernate annotations to define which table and columns should be used to save the Post Object.
 @Entity
@@ -22,6 +25,9 @@ public class Post {
 
 //    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
 //    private List<Comment> comments = new ArrayList<>();
+
+    @Column(name = "author")
+    private String username;
 
     public Post() {
     }
@@ -44,6 +50,14 @@ public class Post {
 
     public void setBody(String body) {
         this.body = body;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
 //    public void addComment(Comment comment) {
